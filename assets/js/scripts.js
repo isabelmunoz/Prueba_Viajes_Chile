dadf00fc072cc7a82d18eb28f7a346a0ae787b32
Isabel Muñoz <36016296+isabelmunoz@users.noreply.github.com>
2022-04-13 18:21:11 -0400

    otro efecto con jquery, documentacion de versiones

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,5 @@
+/* realiza el efecto smooth entre el menú y body*/
+
 const links = document.querySelectorAll(".navbar-nav a");
 
 for (const link of links) {
@@ -15,6 +17,29 @@ function clickHandler(e) {
     });
 }
 
+
+/* esconde el contenido de las card*/
 $( ".card-title" ).click(function() {
     $( ".card-text" ).toggle();
   });
+
+
+	
+      $("#icon_1").mouseover(function(event){
+        $("#icon_1").addClass("cambio_size");
+     });
+     $("#icon_1").mouseout(function(event){
+        $("#icon_1").removeClass("cambio_size");
+     });
+     $("#icon_2").mouseover(function(event){
+        $("#icon_2").addClass("cambio_size");
+     });
+     $("#icon_2").mouseout(function(event){
+        $("#icon_2").removeClass("cambio_size");
+     });
+     $("#icon_3").mouseover(function(event){
+        $("#icon_3").addClass("cambio_size");
+     });
+     $("#icon_3").mouseout(function(event){
+        $("#icon_3").removeClass("cambio_size");
+     });
